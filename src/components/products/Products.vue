b61c15a3d25d3523b1e53d5dc3224516f2d96b64
riduanmatin <riduansyahmatin@gmail.com>
2022-10-15 11:57:02 +0700

    products page update

--- src/components/products/Products.vue
+++ src/components/products/Products.vue
@@ -7,264 +7,124 @@
     <div class="prodCards">
       <div class="row-1 flex justify-center p-5">
         <div
-          class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
-          style="width: 100%"
+          class="cards w-full max-w-sm bg-white mx-2 h-1/3 p-14 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
         >
           <a href="#">
             <img
-              class="rounded-t-lg"
-              src="../../assets/images/fruit bland pineapple + WM.jpg"
-              alt=""
+              class="p-8 rounded-t-lg"
+              src="../../assets/images/fruit bland kiwi.jpg"
+              alt="product image"
             />
           </a>
-          <div class="p-5">
+          <div class="px-5 pb-5">
             <a href="#">
               <h5
-                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+                class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
               >
-                Noteworthy technology acquisitions 2021
+                Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
               </h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <a
-              href="#"
-              class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Read more
-              <svg
-                aria-hidden="true"
-                class="ml-2 -mr-1 w-4 h-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
             </a>
           </div>
         </div>
         <div
-          class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+          class="cards w-full max-w-sm bg-white mx-2 h-1/3 p-14 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
         >
           <a href="#">
             <img
-              class="rounded-t-lg"
-              src="../../assets/images/fruit bland pineapple + WM.jpg"
-              alt=""
+              class="p-8 rounded-t-lg"
+              src="../../assets/images/fruit bland kiwi.jpg"
+              alt="product image"
             />
           </a>
-          <div class="p-5">
+          <div class="px-5 pb-5">
             <a href="#">
               <h5
-                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+                class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
               >
-                Noteworthy technology acquisitions 2021
+                Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
               </h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <a
-              href="#"
-              class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Read more
-              <svg
-                aria-hidden="true"
-                class="ml-2 -mr-1 w-4 h-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
             </a>
           </div>
         </div>
         <div
-          class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+          class="cards w-full max-w-sm bg-white mx-2 h-1/3 p-14 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
         >
           <a href="#">
             <img
-              class="rounded-t-lg"
-              src="../../assets/images/fruit bland pineapple + WM.jpg"
-              alt=""
+              class="p-8 rounded-t-lg"
+              src="../../assets/images/fruit bland kiwi.jpg"
+              alt="product image"
             />
           </a>
-          <div class="p-5">
+          <div class="px-5 pb-5">
             <a href="#">
               <h5
-                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+                class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
               >
-                Noteworthy technology acquisitions 2021
+                Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
               </h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <a
-              href="#"
-              class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Read more
-              <svg
-                aria-hidden="true"
-                class="ml-2 -mr-1 w-4 h-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
             </a>
           </div>
         </div>
       </div>
       <div class="row-1 flex justify-center p-5">
         <div
-          class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
-          style="width: 100%"
+          class="cards w-full max-w-sm bg-white mx-2 h-1/3 p-14 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
         >
           <a href="#">
             <img
-              class="rounded-t-lg"
-              src="../../assets/images/fruit bland pineapple + WM.jpg"
-              alt=""
+              class="p-8 rounded-t-lg"
+              src="../../assets/images/fruit bland kiwi.jpg"
+              alt="product image"
             />
           </a>
-          <div class="p-5">
+          <div class="px-5 pb-5">
             <a href="#">
               <h5
-                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+                class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
               >
-                Noteworthy technology acquisitions 2021
+                Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
               </h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <a
-              href="#"
-              class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Read more
-              <svg
-                aria-hidden="true"
-                class="ml-2 -mr-1 w-4 h-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
             </a>
           </div>
         </div>
         <div
-          class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+          class="cards w-full max-w-sm bg-white mx-2 h-1/3 p-14 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
         >
           <a href="#">
             <img
-              class="rounded-t-lg"
-              src="../../assets/images/fruit bland pineapple + WM.jpg"
-              alt=""
+              class="p-8 rounded-t-lg"
+              src="../../assets/images/fruit bland kiwi.jpg"
+              alt="product image"
             />
           </a>
-          <div class="p-5">
+          <div class="px-5 pb-5">
             <a href="#">
               <h5
-                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+                class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
               >
-                Noteworthy technology acquisitions 2021
+                Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
               </h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <a
-              href="#"
-              class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Read more
-              <svg
-                aria-hidden="true"
-                class="ml-2 -mr-1 w-4 h-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
             </a>
           </div>
         </div>
         <div
-          class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+          class="cards w-full max-w-sm bg-white mx-2 h-1/3 p-14 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
         >
           <a href="#">
             <img
-              class="rounded-t-lg"
-              src="../../assets/images/fruit bland pineapple + WM.jpg"
-              alt=""
+              class="p-8 rounded-t-lg"
+              src="../../assets/images/fruit bland kiwi.jpg"
+              alt="product image"
             />
           </a>
-          <div class="p-5">
+          <div class="px-5 pb-5">
             <a href="#">
               <h5
-                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+                class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
               >
-                Noteworthy technology acquisitions 2021
+                Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
               </h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-            <a
-              href="#"
-              class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Read more
-              <svg
-                aria-hidden="true"
-                class="ml-2 -mr-1 w-4 h-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
             </a>
           </div>
         </div>
@@ -287,5 +147,9 @@ export default {
   width: 100%;
   height: 40vh;
   padding: 30vh 15vw;
+}
+
+.cards {
+  background-color: rgba(152, 152, 152, 1);
 }
 </style>
