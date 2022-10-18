67c494d087d9932f6cc58f6fae4cfea82f7f2f1c
riduanmatin <riduansyahmatin@gmail.com>
2022-10-18 13:38:15 +0700

    about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <h1>ABOUT US</h1>
+  <div class="about flex justify-between">
+    <div class="w-1/2 p-10">
+      <img src="../assets/images/Genova_Logo_no_bg.png" alt=""/>
+    </div>
+    <div class="w-1/2 desc p-20 mx-10 text-left">
+      <p>
+        GENOVA adalah perusahaan yang bergerak memproduksi Premium : Sirup,
+        Powder Drink dan Fruitblend dengan harga yang terjangkau.<br /><br />
+
+        Menggunakan bahan baku impor berkualitas, bersertifikasi halal serta
+        diproduksi di Jakarta, Indonesia. <br />
+        Sehingga ketersediaan stock untuk pasokan bahan baku minuman selalu
+        terjaga.<br /><br />
+
+        Produk Genova juga sudah memiliki sertifikat BPOM di beberapa produk
+        unggulan dan terus mengupdate semua produknya agar memiliki sertifikat
+        BPOM. Sehingga produk kami aman untuk digunakan sebagai bahan baku
+        minuman<br /><br />
+
+        GENOVA saat ini sudah dipercaya oleh lebih dari 1000 pelanggan setia
+        dari hotel bintang lima maupun cafe papan atas di seluruh Indonesia.<br /><br />
+
+        Product GENOVA banyak digunakan untuk campuran pembuatan mocktail,
+        cocktail, milkshake, ice blend, hot drink dan lain-lain. Tersedia dalam
+        ragam varian rasa.<br /><br />
+      </p>
+    </div>
   </div>
 </template>
 
