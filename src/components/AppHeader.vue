e9801ccc9b6a2323a1847f91c033a8a706a6ce4b
riduanmatin <riduansyahmatin@gmail.com>
2022-10-23 12:13:53 +0700

    update auto hide navbar mobile

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -36,19 +36,34 @@
           :class="showMenu ? 'flex' : 'hidden'"
           class="flex-col mt-8 space-y-4 lg:flex lg:space-y-0 lg:flex-row lg:items-center lg:space-x-10 lg:mt-0 lg:order-1"
         >
-          <li class="text-base font-semibold text-gray-800 hover:text-blue-400">
+          <li
+            @click="showMenu = !showMenu"
+            class="text-base font-semibold text-gray-800 hover:text-blue-400"
+          >
             <router-link to="/products">Product</router-link>
           </li>
-          <li class="text-base font-semibold text-gray-800 hover:text-blue-400">
+          <li
+            @click="showMenu = !showMenu"
+            class="text-base font-semibold text-gray-800 hover:text-blue-400"
+          >
             <router-link to="/recipe">Recipe</router-link>
           </li>
-          <li class="text-base font-semibold text-gray-800 hover:text-blue-400">
+          <li
+            @click="showMenu = !showMenu"
+            class="text-base font-semibold text-gray-800 hover:text-blue-400"
+          >
             <router-link to="/about">About Us</router-link>
           </li>
-          <li class="text-base font-semibold text-gray-800 hover:text-blue-400">
+          <li
+            @click="showMenu = !showMenu"
+            class="text-base font-semibold text-gray-800 hover:text-blue-400"
+          >
             <router-link to="/contact">Contact</router-link>
           </li>
-          <li class="text-base font-semibold text-gray-800 hover:text-blue-400">
+          <li
+            @click="showMenu = !showMenu"
+            class="text-base font-semibold text-gray-800 hover:text-blue-400"
+          >
             <router-link to="/location">Location</router-link>
           </li>
         </ul>
